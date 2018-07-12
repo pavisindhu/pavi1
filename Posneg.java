693abe5dbbe038c040da7b837ee9142f3090b035
pavisindhu <41136803+pavisindhu@users.noreply.github.com>
2018-07-12 10:28:23 +0530

    Add files via upload

---
+++ Posneg.java
@@ -0,0 +1,15 @@
+public class Posneg {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=Integer.parseInt(args[0]);
+		if(a<0){
+			System.out.println("the no is negative");
+		}
+		else if(a>0){
+			System.out.println("the no is postive");
+		}
+		else{
+			System.out.println("zero");
+		}
+	}
+}
